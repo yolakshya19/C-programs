3c97c33526bc14781e8d70038ba132a7819449bf
Lakshya Dhawan <89843481+yolakshya19@users.noreply.github.com>
2022-07-28 19:43:57 +0530

    Add files via upload

---
+++ concatenate.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+void main()
+{
+  char str1[25],str2[25];
+  int i=0,j=0;
+  printf("Enter First String: ");
+  gets(str1);
+  printf("Enter Second String: ");
+  gets(str2);
+  while(str1[i]!='\0')
+  {
+      i++;
+  }
+  str1[i]=' ';
+  while(str2[j]!='\0')
+  {
+    str1[i]=str2[j];
+    j++;
+    i++;
+  }
+  str1[i]='\0';
+  printf("\nConcatenated String is: %s",str1);
+}
