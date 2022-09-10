88b70332c995a0c789593278f12774a0bd609e08
Lakshya Dhawan <89843481+yolakshya19@users.noreply.github.com>
2022-09-10 21:49:08 +0530

    Add files via upload

---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,8 @@
+deleteNode(&head,30);
+     printf("Deleted 30. The New Linked List:\n");
+     printList(head);
+
+     //delete 20
+     deleteNode(&head,20);
+     printf("Deleted 20. The New Linked List:\n");
+     printList(head);
